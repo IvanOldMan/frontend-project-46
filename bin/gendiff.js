c4eabad9f62ed589ed4f670e718570dcee1aa308
Ivan Starikov <oldstarman@yandex.ru>
2024-05-18 21:44:46 +0300

    step_2: fixed bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,10 @@ const program = new Command();
 program
 	.name('gendiff')
 	.description('Compares two configuration files and shows a difference.')
+	.argument('<filepath1>')
+	.argument('<filepath2>')
 	.version('0.0.1')
+	.option('-f, --format [type]', 'output format')
 	.helpOption('-h, --help', 'output usage information');
 
 program.parse();

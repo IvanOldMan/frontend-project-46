34d70bb61963b13c6044527733f6588141f36b08
Ivan Starikov <oldstarman@yandex.ru>
2024-06-03 23:38:52 +0300

    final_step: fix

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -13,28 +13,25 @@ const readFile = (filepath) => fs.readFileSync(getFixturePath(filepath), 'utf-8'
 const extList = ['.json', '.yaml', '.yml'];
 
 test.only.each(extList)('test stylish', (type) => {
+  const filepath1 = getFixturePath(`file1${type}`);
+  const filepath2 = getFixturePath(`file2${type}`);
+  const result = readFile('stylish_result.txt');
 
-	const filepath1 = getFixturePath(`file1${type}`);
-	const filepath2 = getFixturePath(`file2${type}`);
-	const result = readFile('stylish_result.txt');
-
-	expect(gendiff(filepath1, filepath2, 'stylish')).toEqual(result);
+  expect(gendiff(filepath1, filepath2, 'stylish')).toEqual(result);
 });
 
-test.only.each(extList)(`test plain`, (type) => {
+test.only.each(extList)('test plain', (type) => {
+  const filepath1 = getFixturePath(`file1${type}`);
+  const filepath2 = getFixturePath(`file2${type}`);
+  const result = readFile('plain_result.txt');
 
-	const filepath1 = getFixturePath(`file1${type}`);
-	const filepath2 = getFixturePath(`file2${type}`);
-	const result = readFile('plain_result.txt');
-
-	expect(gendiff(filepath1, filepath2, 'plain')).toEqual(result);
+  expect(gendiff(filepath1, filepath2, 'plain')).toEqual(result);
 });
 
-test.only.each(extList)(`test json`, (type) => {
+test.only.each(extList)('test json', (type) => {
+  const filepath1 = getFixturePath(`file1${type}`);
+  const filepath2 = getFixturePath(`file2${type}`);
+  const result = readFile('json_result.txt');
 
-	const filepath1 = getFixturePath(`file1${type}`);
-	const filepath2 = getFixturePath(`file2${type}`);
-	const result = readFile('json_result.txt');
-
-	expect(gendiff(filepath1, filepath2, 'json')).toEqual(result);
+  expect(gendiff(filepath1, filepath2, 'json')).toEqual(result);
 });
